39b8be3829b99f271a0bb97273cc1c5d71b01b63
luletteiriarte <carolina.iriarte@correo.ucu.edu.uy>
2026-02-23 09:18:52 -0300

    Actualización de Tipos

--- src/app/types/Bid.ts
+++ src/app/types/Bid.ts
@@ -1,3 +1,15 @@
+
+export interface Familia {
+  id_familia: number;
+  nombre: string;
+}
+
+export interface Subfamilia {
+  id_subfamilia: number;
+  nombre: string;
+  id_familia: number;
+}
+
 export interface Bid {
   id_licitacion: number;
   title: string;
@@ -5,8 +17,6 @@ export interface Bid {
   fecha_publicacion: string; // YYYY-MM-DD
   fecha_cierre: string; // YYYY-MM-DDTHH:MM:SS
   link: string;
-  familia: { id_familia: number; nombre: string };
-  subfamilia: { id_subfamilia: number; nombre: string };
-  clase: { id_clase: number; nombre: string };
-  subclase: { id_subclase: number; nombre: string };
+  familia: Familia;
+  subfamilia: Subfamilia;
 }
